8c60baa816bb3a15ab8f6e149b10132cca1e61fc
rustamsobirov <carwolt123@gmail.com>
2022-05-11 12:21:35 +0500

    some problem fixed

--- app/src/main/java/me/ruyeo/newcut/ui/BaseFragment.kt
+++ app/src/main/java/me/ruyeo/newcut/ui/BaseFragment.kt
@@ -1,9 +1,21 @@
 package me.ruyeo.newcut.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import me.ruyeo.newcut.utils.dialogs.MessageDialog
 
 abstract class BaseFragment(private val layoutRes: Int): Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(layoutRes, container, false)
+    }
 
     protected fun showMessage(message: String) {
         val dialog = MessageDialog(message)
