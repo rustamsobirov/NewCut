3b4b8f8336cc8c0feee4f773c54c927b950b215a
Mirkamol <mirkamol9187907@gmail.com>
2022-05-13 23:48:04 +0500

    Login fragment_login.xml finished

--- app/src/main/java/me/ruyeo/newcut/ui/auth/ConfirmationFragment.kt
+++ app/src/main/java/me/ruyeo/newcut/ui/auth/ConfirmationFragment.kt
@@ -1,10 +1,7 @@
 package me.ruyeo.newcut.ui.auth
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import dagger.hilt.android.AndroidEntryPoint
 import me.ruyeo.newcut.R
 import me.ruyeo.newcut.databinding.FragmentConfirmationBinding
@@ -17,5 +14,11 @@ class ConfirmationFragment : BaseFragment(R.layout.fragment_confirmation) {
     private val binding by viewBinding { FragmentConfirmationBinding.bind(it) }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initViews(view)
+    }
+
+    fun initViews(view: View) {
+
     }
 }
