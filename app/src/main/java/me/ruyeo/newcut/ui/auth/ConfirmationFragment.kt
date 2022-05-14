65b812538bbf90a4691019d6ff207b8ca2601554
Mirkamol <mirkamol9187907@gmail.com>
2022-05-15 02:35:00 +0500

    ConfirmationFragment.kt has been customized

--- app/src/main/java/me/ruyeo/newcut/ui/auth/ConfirmationFragment.kt
+++ app/src/main/java/me/ruyeo/newcut/ui/auth/ConfirmationFragment.kt
@@ -1,7 +1,18 @@
 package me.ruyeo.newcut.ui.auth
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.getSystemService
+import androidx.core.widget.addTextChangedListener
+import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import me.ruyeo.newcut.R
 import me.ruyeo.newcut.databinding.FragmentConfirmationBinding
@@ -10,15 +21,125 @@ import me.ruyeo.newcut.utils.extensions.viewBinding
 
 @AndroidEntryPoint
 class ConfirmationFragment : BaseFragment(R.layout.fragment_confirmation) {
+    companion object {
+        private const val TEST_VERIFY_CODE = "1234"
+    }
 
+    private val EditTextOne: EditText by lazy {
+        binding.ed1
+    }
+    private val EditTextTwo: EditText by lazy {
+        binding.ed2
+    }
+    private val EditTextThree: EditText by lazy {
+        binding.ed3
+    }
+    private val EditTextFour: EditText by lazy {
+        binding.ed4
+    }
     private val binding by viewBinding { FragmentConfirmationBinding.bind(it) }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        initViews(view)
-    }
-
-    fun initViews(view: View) {
+        onBack()
+        setListener()
+        initFocus()
 
     }
+
+    private fun onBack() {
+        binding.ivBack.setOnClickListener {
+            findNavController().navigate(R.id.action_confirmationFragment_to_loginFragment)
+        }
+    }
+
+    private fun setListener() {
+
+        setTextChengeListener(fromEditText = EditTextOne, targetEdittext = EditTextTwo)
+        setTextChengeListener(fromEditText = EditTextTwo, targetEdittext = EditTextThree)
+        setTextChengeListener(fromEditText = EditTextThree, targetEdittext = EditTextFour)
+        setTextChengeListener(fromEditText = EditTextFour, done = {
+            verifyOTPCode()
+        })
+
+        setKeyListener(fromEditText = EditTextTwo, backToEditText = EditTextOne)
+        setKeyListener(fromEditText = EditTextThree, backToEditText = EditTextTwo)
+        setKeyListener(fromEditText = EditTextFour, backToEditText = EditTextThree)
+    }
+
+    @SuppressLint("ServiceCast")
+    private fun initFocus() {
+        EditTextOne.isEnabled = true
+
+        EditTextOne.postDelayed({
+            EditTextOne.requestFocus()
+        }, 500)
+    }
+
+    private fun setTextChengeListener(
+        fromEditText: EditText,
+        targetEdittext: EditText? = null,
+        done: (() -> Unit)? = null
+    ) {
+        fromEditText.addTextChangedListener {
+            it?.let { string ->
+                if (string.isNotEmpty()) {
+                    targetEdittext?.let { edittext ->
+                        edittext.isEnabled = true
+                        edittext.requestFocus()
+                    } ?: run {
+                        done?.let { done ->
+                            done()
+                        }
+
+                    }
+
+                    fromEditText.clearFocus()
+                    fromEditText.isEnabled = false
+                }
+            }
+        }
+
+    }
+
+    private fun setKeyListener(fromEditText: EditText, backToEditText: EditText) {
+        fromEditText.setOnKeyListener { _, _, event ->
+            if (null != event && KeyEvent.KEYCODE_DEL == event.keyCode) {
+                backToEditText.isEnabled = true
+                backToEditText.requestFocus()
+                backToEditText.setText("")
+
+
+                fromEditText.clearFocus()
+                fromEditText.isEnabled = false
+            }
+            false
+
+        }
+    }
+
+    private fun verifyOTPCode() {
+        val otpCode =
+            "${EditTextOne.text.toString().trim()}" +
+                    "${EditTextTwo.text.toString().trim()}" +
+                    "${EditTextThree.text.toString().trim()}" +
+                    "${EditTextFour.text.toString().trim()}"
+
+        if (4 != otpCode.length) {
+            return
+        }
+        if (otpCode == TEST_VERIFY_CODE) {
+            EditTextOne.setBackgroundResource(R.drawable.code_background)
+            EditTextTwo.setBackgroundResource(R.drawable.code_background)
+            EditTextThree.setBackgroundResource(R.drawable.code_background)
+            EditTextFour.setBackgroundResource(R.drawable.code_background)
+
+            Toast.makeText(context, "Success, should do next", Toast.LENGTH_SHORT).show()
+
+            return
+        }
+
+        Toast.makeText(context, "error, input again", Toast.LENGTH_SHORT).show()
+
+    }
+
 }
--- app/src/main/java/me/ruyeo/newcut/ui/auth/LoginFragment.kt
+++ app/src/main/java/me/ruyeo/newcut/ui/auth/LoginFragment.kt
@@ -2,6 +2,7 @@ package me.ruyeo.newcut.ui.auth
 
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import me.ruyeo.newcut.R
 import me.ruyeo.newcut.databinding.FragmentLoginBinding
@@ -14,6 +15,12 @@ class LoginFragment : BaseFragment(R.layout.fragment_login) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.continueBtn.setOnClickListener {
+            if (binding.phoneNumberEdt.text.isNotEmpty()){
+                findNavController().navigate(R.id.action_loginFragment_to_confirmationFragment)
+            }
+        }
 
     }
 
