76a6eac172efc64fc21bf900224c26aac8141460
Abdujabbor17 <jabborprimqulov@gmail.com>
2022-05-16 10:47:13 +0500

    bug fixed

---
+++ app/src/main/java/me/ruyeo/newcut/adapter/UpcomingAppointmentAdapter.kt
@@ -0,0 +1,14 @@
+package me.ruyeo.newcut.adapter
+
+import androidx.recyclerview.widget.RecyclerView
+import me.ruyeo.newcut.databinding.ItemUpcomingAppointmentBinding
+
+class UpcomingAppointmentAdapter() {
+
+    inner class Vh(var itemUpcomingAppointmentBinding: ItemUpcomingAppointmentBinding) :
+        RecyclerView.ViewHolder(itemUpcomingAppointmentBinding.root)
+
+    fun bind(){
+
+    }
+}
