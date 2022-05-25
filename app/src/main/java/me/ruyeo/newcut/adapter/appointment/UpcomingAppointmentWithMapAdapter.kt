86317f282d26b3e14b0bd1a5583f8bd065247615
Abdujabbor17 <jabborprimqulov@gmail.com>
2022-05-25 12:38:32 +0500

    added profile fragments

--- app/src/main/java/me/ruyeo/newcut/adapter/appointment/UpcomingAppointmentWithMapAdapter.kt
+++ app/src/main/java/me/ruyeo/newcut/adapter/appointment/UpcomingAppointmentWithMapAdapter.kt
@@ -1,10 +1,15 @@
 package me.ruyeo.newcut.adapter.appointment
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import me.ruyeo.newcut.App
+import me.ruyeo.newcut.R
 import me.ruyeo.newcut.databinding.ItemAppointmentWithMapBinding
 import me.ruyeo.newcut.model.appointment.UpcomingAppointment
 import me.ruyeo.newcut.model.appointment.UpcomingAppointmentMapModel
@@ -14,10 +19,29 @@ class UpcomingAppointmentWithMapAdapter:RecyclerView.Adapter<UpcomingAppointment
     var spinnerClick: (() -> Unit)? = null
 
     inner class Vh(var binding:ItemAppointmentWithMapBinding):RecyclerView.ViewHolder(binding.root){
-        fun bind(){
+        fun bind() {
+            val adapter: ArrayAdapter<CharSequence> = ArrayAdapter.createFromResource(
+                App.instance,
+                R.array.times,
+                R.layout.item_spinner_list
+            )
+            adapter.setDropDownViewResource(R.layout.item_spinner_list)
 
+            binding.apply {
+                spinner.adapter = adapter
+                spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                        spinnerClick?.invoke()
+                    }
 
+                    override fun onNothingSelected(p0: AdapterView<*>?) {
+
+                    }
+
+                }
+            }
         }
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UpcomingAppointmentWithMapAdapter.Vh {
