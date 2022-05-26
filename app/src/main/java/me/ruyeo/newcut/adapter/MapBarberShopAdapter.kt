4a850880ba76b3f6b44b4adfefbaf29a85d657da
ConaMobile <conamobiledev@gmail.com>
2022-05-26 16:05:02 +0500

    map polyline

--- app/src/main/java/me/ruyeo/newcut/adapter/MapBarberShopAdapter.kt
+++ app/src/main/java/me/ruyeo/newcut/adapter/MapBarberShopAdapter.kt
@@ -1,8 +1,6 @@
 package me.ruyeo.newcut.adapter
 
-import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
