ec58009bd44b4c2cd90be3736b59972feb850c93
Abdujabbor17 <jabborprimqulov@gmail.com>
2022-05-26 12:54:12 +0500

    bug fixed

--- app/src/main/java/me/ruyeo/newcut/ui/client/profile/EditProfileFragment.kt
+++ app/src/main/java/me/ruyeo/newcut/ui/client/profile/EditProfileFragment.kt
@@ -2,16 +2,13 @@ package me.ruyeo.newcut.ui.client.profile
 
 import android.annotation.SuppressLint
 import android.app.DatePickerDialog
-import android.icu.text.DateFormat.DAY
-import android.icu.text.DateFormat.MONTH
-import android.icu.text.DateTimePatternGenerator.DAY
-import android.icu.util.MeasureUnit.DAY
-import android.os.Build
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import androidx.annotation.RequiresApi
+import android.widget.DatePicker
 import dagger.hilt.android.AndroidEntryPoint
 import me.ruyeo.newcut.R
 import me.ruyeo.newcut.databinding.FragmentEditProfileBinding
@@ -21,32 +18,37 @@ import java.util.*
 
 @AndroidEntryPoint
 class EditProfileFragment : BaseFragment(R.layout.fragment_edit_profile),
-    AdapterView.OnItemSelectedListener {
+    AdapterView.OnItemSelectedListener,DatePickerDialog.OnDateSetListener{
     private val binding by viewBinding { FragmentEditProfileBinding.bind(it) }
 
     @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         // Calendar
-        val c= Calendar.getInstance()
-        val year=c.get(Calendar.YEAR)
-        val month=c.get(Calendar.MONTH)
-        val day= c.get(Calendar.DAY_OF_MONTH)
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
 
         binding.apply {
             birthdayTv.setOnClickListener {
-                val datePickerDialog = DatePickerDialog(requireContext(),
-                    DatePickerDialog.OnDateSetListener{view,mYear,mMonth,mDay->
-                        birthdayTv.text = "$mDay/$mMonth/$mYear"
-                    },year,month,day)
+                val datePickerDialog = DatePickerDialog(
+                    requireContext(),
+                    android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                    this@EditProfileFragment,
+                    year,month,day
+                )
+                datePickerDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
                 datePickerDialog.show()
             }
         }
 
 
-        val adapter: ArrayAdapter<CharSequence> = ArrayAdapter.createFromResource(requireContext(),
-            R.array.gender,R.layout.item_spinner_list)
-        adapter.setDropDownViewResource(R.layout.item_spinner_list)
+        val adapter: ArrayAdapter<CharSequence> = ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.gender, R.layout.item_spinner_list_gender
+        )
+        adapter.setDropDownViewResource(R.layout.item_spinner_list_gender)
         binding.spinnerGender.adapter = adapter
 
         binding.spinnerGender.onItemSelectedListener = this
@@ -58,6 +60,16 @@ class EditProfileFragment : BaseFragment(R.layout.fragment_edit_profile),
     }
 
     override fun onNothingSelected(p0: AdapterView<*>?) {
+
+    }
+
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+
+        val day = String.format("%02d",dayOfMonth)
+        val mMonth = String.format("%02d",month)
+      val date = "$day/$mMonth/$year"
+        binding.birthdayTv.text = date
 
     }
 
